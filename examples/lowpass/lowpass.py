aa4be733d62459028cbe39cde3752019be6b2a80
Endolith <endolith@gmail.com>
2017-02-05 22:52:16 -0500

    DOC: Improve lowpass example plot
    Add grids, sharex
    
    Change x margins to 0 for MPL 2.0
    
    Updated image with all changes
    
    Use Pythonic ac() command instead of cmd()

--- examples/lowpass/lowpass.py
+++ examples/lowpass/lowpass.py
@@ -11,24 +11,25 @@ ngspyce.cmd('source lowpass.net')
 
 # Calculate small-signal transfer function between 1 kHz and 10 MHz, with 5
 # points per decade
-ngspyce.cmd('ac dec 5 1k 10meg')
+ngspyce.ac(mode='dec', npoints=5, fstart=1e3, fstop=10e6)
 
 # Read results
 freq = np.abs(ngspyce.vector('frequency'))
 vout = ngspyce.vector('vout')
 
 # And plot them
-fig = plt.figure()
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)
 fig.suptitle('Frequency response of an RC low-pass filter')
 
-ax = fig.add_subplot(2, 1, 1)
-ax.semilogx(freq, 20*np.log10(np.abs(vout)))
-ax.set_ylabel('Magnitude [dB]')
+ax1.semilogx(freq, 20*np.log10(np.abs(vout)))
+ax1.set_ylabel('Magnitude [dB]')
+ax1.grid(True, which='both')
 
-ax = fig.add_subplot(2, 1, 2)
-ax.semilogx(freq, np.angle(vout, True))
-ax.set_xlabel('Frequency [Hz]')
-ax.set_ylabel('Phase [degrees]')
+ax2.semilogx(freq, np.angle(vout, True))
+ax2.set_xlabel('Frequency [Hz]')
+ax2.set_ylabel('Phase [degrees]')
+ax2.grid(True, which='both')
+ax2.margins(x=0)
 
 plt.savefig('lowpass.png')
 plt.show()
