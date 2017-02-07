4135544ac114b9213330fba400870cbe6bbaee98
Endolith <endolith@gmail.com>
2017-02-06 23:54:47 -0500

    PEP8 formatting, spaces, etc. API: vector_names
    Includes backwards incompatible change: vectorNames -> vector_names
    
    "Function names should be lowercase, with words separated by underscores
    as necessary to improve readability.
    
    mixedCase is allowed only in contexts where that's already the
    prevailing style (e.g. threading.py), to retain backwards
    compatibility."

--- ngspyce/ngspyce.py
+++ ngspyce/ngspyce.py
@@ -1,29 +1,29 @@
-
-from ctypes import CDLL, CFUNCTYPE, Structure, c_int, c_char_p, c_void_p, \
-        c_bool, c_double, POINTER, pointer, cast, c_short, py_object
+from ctypes import (CDLL, CFUNCTYPE, Structure, c_int, c_char_p, c_void_p,
+                    c_bool, c_double, POINTER, pointer, cast, c_short,
+                    py_object)
 import numpy as np
 import logging
 import os
 import platform
 
 __all__ = [
-     'cmd'
-    ,'circ'
-    ,'plots'
-    ,'vectorNames'
-    ,'vectors'
-    ,'vector'
-    ,'try_float'
-    ,'model_parameters'
-    ,'alter_model'
-    ,'ac'
-    ,'dc'
-    ,'operating_point'
-    ,'save'
-    ,'destroy'
-    ,'decibel'
-    ,'alter'
-    ,'linear_sweep'
+    'cmd',
+    'circ',
+    'plots',
+    'vector_names',
+    'vectors',
+    'vector',
+    'try_float',
+    'model_parameters',
+    'alter_model',
+    'ac',
+    'dc',
+    'operating_point',
+    'save',
+    'destroy',
+    'decibel',
+    'alter',
+    'linear_sweep',
 ]
 
 logger = logging.getLogger(__name__)
@@ -62,6 +62,7 @@ else:  # Linux, etc.
 
 captured_output = []
 
+
 @CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
 def printfcn(output, id, ret):
     """Callback for libngspice to print a message"""
@@ -73,20 +74,24 @@ def printfcn(output, id, ret):
         captured_output.append(content)
     return 0
 
+
 @CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
-def statfcn(status, id, ret): 
-    """Callback for libngspice to report simulation status like 'tran 5%'"""
+def statfcn(status, id, ret):
+    """
+    Callback for libngspice to report simulation status like 'tran 5%'
+    """
     logger.debug(status.decode('ascii'))
     return 0
+
 
 @CFUNCTYPE(c_int, c_int, c_bool, c_bool, c_int, c_void_p)
 def controlled_exit(exit_status, immediate_unloading, requested_exit,
                     libngspice_id, ret):
-    logger.debug('ControlledExit', dict(exit_status=exit_status,
-                                      immediate_unloading=immediate_unloading,
-                                      requested_exit=requested_exit,
-                                      libngspice_id=libngspice_id,
-                                      ret=ret))
+    logger.debug('ControlledExit',
+                 dict(exit_status=exit_status,
+                      immediate_unloading=immediate_unloading,
+                      requested_exit=requested_exit,
+                      libngspice_id=libngspice_id, ret=ret))
 
 # typedef struct vecvalues {
     # char* name; /* name of a specific vector */
@@ -95,6 +100,8 @@ def controlled_exit(exit_status, immediate_unloading, requested_exit,
     # bool is_scale;/* if 'name' is the scale vector */
     # bool is_complex;/* if the data are complex numbers */
 # } vecvalues, *pvecvalues;
+
+
 class vecvalues(Structure):
     _fields_ = [
         ('name', c_char_p),
@@ -108,11 +115,15 @@ class vecvalues(Structure):
     # int vecindex; /* index of actual set of vectors. i.e. the number of accepted data points */
     # pvecvalues *vecsa; /* values of actual set of vectors, indexed from 0 to veccount - 1 */
 # } vecvaluesall, *pvecvaluesall;
+
+
 class vecvaluesall(Structure):
     _fields_ = [
         ('veccount', c_int),
         ('vecindex', c_int),
         ('vecsa', POINTER(POINTER(vecvalues)))]
+
+
 @CFUNCTYPE(c_int, POINTER(vecvaluesall), c_int, c_int, c_void_p)
 def send_data(vecvaluesall, num_structs, libngspice_id, ret):
     logger.debug('SendData', dict(vecvaluesall=vecvaluesall,
@@ -120,14 +131,17 @@ def send_data(vecvaluesall, num_structs, libngspice_id, ret):
                                   libngspice_id=libngspice_id,
                                   ret=ret))
 
-spice.ngSpice_Init(printfcn, statfcn, controlled_exit, send_data, None, None, 
+spice.ngSpice_Init(printfcn, statfcn, controlled_exit, send_data, None, None,
                    None)
 
 # int  ngSpice_Command(char* command);
 spice.ngSpice_Command.argtypes = [c_char_p]
 
+
 def cmd(command):
-    """Send a commang to the ngspice engine"""
+    """
+    Send a command to the ngspice engine
+    """
     max_length = 1023
     if len(command) > max_length:
         raise Exception('Command length', len(command), 'greater than',
@@ -139,10 +153,10 @@ def cmd(command):
 
 # int ngSpice_Circ(char**)
 spice.ngSpice_Circ.argtypes = [POINTER(c_char_p)]
-
 def circ(netlist_lines):
-    """Specify a netlist
-    
+    """
+    Specify a netlist
+
     Accepts an array of lines, or a multi-line string
     """
     if issubclass(type(netlist_lines), str):
@@ -164,7 +178,9 @@ def circ(netlist_lines):
 
 spice.ngSpice_AllPlots.restype = POINTER(c_char_p)
 def plots():
-    """List available plots (result sets)"""
+    """
+    List available plots (result sets)
+    """
     ret = []
     plotlist = spice.ngSpice_AllPlots()
     ii = 0
@@ -174,12 +190,14 @@ def plots():
         ret.append(plotlist[ii].decode('ascii'))
         ii += 1
 
+
 spice.ngSpice_AllVecs.argtypes = [c_char_p]
 spice.ngSpice_AllVecs.restype = POINTER(c_char_p)
 spice.ngSpice_CurPlot.restype = c_char_p
-def vectorNames(plot=None):
-    """List the vectors present in the specified plot 
-    
+def vector_names(plot=None):
+    """
+    List the vectors present in the specified plot
+
     List the voltages, currents, etc present in the specified plot.
     Defaults to the last plot.
     """
@@ -193,6 +211,7 @@ def vectorNames(plot=None):
             return names
         names.append(veclist[ii].decode('ascii'))
         ii += 1
+
 
 class ngcomplex(Structure):
     _fields_ = [
@@ -209,15 +228,17 @@ class ngcomplex(Structure):
 #   VF_MAXGIVEN = (1 << 6),   /* The v_maxsignal value is valid. */
 #   VF_PERMANENT = (1 << 7)   /* Don't garbage collect this vector. */
 # };
+
+
 class dvec_flags(object):
-   vf_real = (1 << 0)       # The data is real. */
-   vf_complex = (1 << 1)    # The data is complex. */
-   vf_accum = (1 << 2)      # writedata should save this vector. */
-   vf_plot = (1 << 3)       # writedata should incrementally plot it. */
-   vf_print = (1 << 4)      # writedata should print this vector. */
-   vf_mingiven = (1 << 5)   # The v_minsignal value is valid. */
-   vf_maxgiven = (1 << 6)   # The v_maxsignal value is valid. */
-   vf_permanent = (1 << 7)  # Don't garbage collect this vector. */
+    vf_real = (1 << 0)       # The data is real.
+    vf_complex = (1 << 1)    # The data is complex.
+    vf_accum = (1 << 2)      # writedata should save this vector.
+    vf_plot = (1 << 3)       # writedata should incrementally plot it.
+    vf_print = (1 << 4)      # writedata should print this vector.
+    vf_mingiven = (1 << 5)   # The v_minsignal value is valid.
+    vf_maxgiven = (1 << 6)   # The v_maxsignal value is valid.
+    vf_permanent = (1 << 7)  # Don't garbage collect this vector.
 
 # /* vector info obtained from any vector in ngspice.dll.
    # Allows direct access to the ngspice internal vector structure,
@@ -230,6 +251,8 @@ class dvec_flags(object):
 #    ngcomplex_t *v_compdata;	/* Complex data. */
 #    int v_length;		/* Length of the vector. */
 #} vector_info, *pvector_info;
+
+
 class vector_info(Structure):
     _fields_ = [
         ('v_name', c_char_p),
@@ -266,22 +289,28 @@ vector_type = [
     'dimensionless'       # phase = 17
     'NotImplemented',     # db = 18
     'farad'               # capacitance = 19
-    'coulomb'             # charge = 21           
+    'coulomb'             # charge = 21
 ]
 
+
 def vectors(names=None):
-    """Return a dictionary with the specified vectors
-    
-    If names is None, return all available vectors"""
+    """
+    Return a dictionary with the specified vectors
+
+    If `names` is None, return all available vectors
+    """
     if names is None:
         plot = spice.ngSpice_CurPlot()
-        names = vectorNames(plot)
+        names = vector_names(plot)
     return dict(zip(names, map(vector, names)))
 
+
 def vector(name, plot=None):
-    """Return a numpy.ndarray with the specified vector
-    
-    Uses the current plot by default."""
+    """
+    Return a numpy.ndarray with the specified vector
+
+    Uses the current plot by default.
+    """
     if plot is not None:
         name = plot + '.' + name
     vec = spice.ngGet_Vec_Info(name.encode('ascii'))
@@ -293,8 +322,8 @@ def vector(name, plot=None):
     elif vec.v_flags & dvec_flags.vf_real:
         array = np.ctypeslib.as_array(vec.v_realdata, shape=(vec.v_length,))
     elif vec.v_flags & dvec_flags.vf_complex:
-        components = np.ctypeslib.as_array(vec.v_compdata, 
-                                           shape=(vec.v_length,2))
+        components = np.ctypeslib.as_array(vec.v_compdata,
+                                           shape=(vec.v_length, 2))
         array = np.ndarray(shape=(vec.v_length,), dtype=np.complex128,
                            buffer=components)
     else:
@@ -328,10 +357,13 @@ class simulation_type(object):
     phase = 17
     db = 18
     capacitance = 19
-    charge = 20           
+    charge = 20
+
 
 def try_float(s):
-    '''Parse s as float if possible, otherwise return s'''
+    """
+    Parse `s` as float if possible, otherwise return `s`.
+    """
     try:
         return float(s)
     except ValueError:
@@ -340,8 +372,11 @@ def try_float(s):
         except ValueError:
             return s
 
+
 def model_parameters(device=None, model=None):
-    '''Return dict with model parameters for device or model'''
+    """
+    Return dict with model parameters for device or model
+    """
     if device is None:
         if model is not None:
             lines = cmd('showmod #' + model.lower())
@@ -357,23 +392,26 @@ def model_parameters(device=None, model=None):
                 for parts in map(str.split, lines)})
     return ret
 
+
 def alter_model(device, **params):
-    for k,v in params.items():
+    for k, v in params.items():
         cmd('altermod {} {} = {:.6e}'.format(device, k, v))
 
+
 def ac(mode, npoints, fstart, fstop):
-    '''Perform small-signal AC analysis
+    """
+    Perform small-signal AC analysis
 
     Examples
     ========
-    Sweep from 1kHz to 10MHz with 3 points per decade
+    Sweep from 1kHz to 10 MHz with 3 points per decade
 
     >>> ac('dec', 3, 1e3, 10e6)
 
-    Sweep from 0 to 20kHz in 21 linearly spaced points
+    Sweep from 0 to 20 kHz in 21 linearly spaced points
 
     >>> ac('lin', 21, 0, 20e3)
-    '''
+    """
     modes = ('dec', 'lin', 'oct')
     if mode.lower() not in modes:
         raise Exception(mode, 'not a valid AC sweep mode', modes)
@@ -383,12 +421,15 @@ def ac(mode, npoints, fstart, fstop):
     cmd('ac {} {} {} {}'.format(mode, npoints, fstart, fstop))
     return vectors()
 
+
 def group(iterable, grouplength):
     return zip(*(iterable[ii::grouplength]
                  for ii in range(grouplength)))
-    
+
+
 def dc(*sweeps):
-    '''Analyze DC transfer function, return vectors with one axis per sweep
+    """
+    Analyze DC transfer function, return vectors with one axis per sweep
 
     sweeps is one or more sequences of (src, start, stop, increment)
     src can be an independent voltage or current source, a resistor, or "TEMP".
@@ -398,46 +439,56 @@ def dc(*sweeps):
     Example
     =======
     >>> dc('va', 0, 1, 1, 'vb', 0, 2, 2)
-    {'a': array([[ 0.,  0.], [ 1.,  1.]]), 
+    {'a': array([[ 0.,  0.], [ 1.,  1.]]),
      'b': array([[ 0.,  2.], [ 0.,  2.]]), ...}
-    '''
+    """
     # TODO: support more than two sweeps
     cmd('dc ' + ' '.join(map(str, sweeps)))
     sweepvalues = [linear_sweep(*sweep[1:])
-        for sweep in group(sweeps, 4)]
+                   for sweep in group(sweeps, 4)]
     sweeplengths = tuple(map(len, sweepvalues))
     ret = {k: v.reshape(sweeplengths, order='F')
-           for k,v in vectors().items()}
+           for k, v in vectors().items()}
     for ii, (name, values) in enumerate(zip(sweeps[::4], sweepvalues)):
         shape = [length if ii == jj else 1
-                    for jj, length in enumerate(sweeplengths)]
+                 for jj, length in enumerate(sweeplengths)]
         ret[name] = values.reshape(shape, order='F')
     return ret
 
+
 def operating_point():
-    '''Analyze DC operating point'''
+    """
+    Analyze DC operating point
+    """
     cmd('op')
     return vectors()
+
 
 def save(vector):
     cmd('save ' + vector)
 
+
 def destroy(plotname='all'):
-    '''Erase plot from memory'''
+    """
+    Erase plot from memory
+    """
     cmd('destroy ' + plotname)
+
 
 def decibel(x):
     return 10. * np.log10(np.abs(x))
 
+
 def alter(device, **parameters):
-    '''Alter device parameters
+    """
+    Alter device parameters
 
     Examples
     ========
     >>> alter('R1', resistance=200)
     >>> alter('vin', ac=2, dc=3)
-    '''
-    for k,v in parameters.items():
+    """
+    for k, v in parameters.items():
         if not isinstance(v, (list, tuple)):
             v = str(v)
         else:
@@ -446,7 +497,9 @@ def alter(device, **parameters):
 
 from sys import float_info
 def linear_sweep(start, stop, step):
-    '''Voltages used in a dc transfer curve analysis linear sweep'''
+    """
+    Voltages used in a dc transfer curve analysis linear sweep
+    """
     if (start > stop and step > 0) or (start < stop and step < 0):
         raise Exception("Can't sweep from", start, 'to', stop, 'with step',
                         step)
--- tests/test_ngspyce.py
+++ tests/test_ngspyce.py
@@ -3,6 +3,7 @@ import unittest
 import ngspyce as ns
 from numpy import pi
 
+
 class NgspiceTest(unittest.TestCase):
     def setUp(self):
         ns.destroy()
@@ -19,37 +20,39 @@ class NgspiceTest(unittest.TestCase):
     def assertVectors(self, vectors):
         self.assertEqualDictOfArray(ns.vectors(), vectors)
 
+
 class TestBasicCircuits(NgspiceTest):
     def test_vsource(self):
         ns.circ('va a 0 dc 1')
         ns.operating_point()
-        self.assertEqual(ns.vectors(), 
-                         {'a'        : [1]
-                         ,'va#branch': [0]})
+        self.assertEqual(ns.vectors(),
+                         {'a'        : [1],
+                          'va#branch': [0]})
 
     def test_resistor(self):
         ns.circ(['va a 0 dc 1', 'r a 0 2'])
         ns.operating_point()
-        self.assertEqual(ns.vectors(), 
-                         {'a'        : [1]
-                         ,'va#branch': [-0.5]})
+        self.assertEqual(ns.vectors(),
+                         {'a'        : [1],
+                          'va#branch': [-0.5]})
 
     def test_capacitor(self):
         ns.circ(['va a 0 ac 1 dc 0', 'c a 0 1'])
         ns.ac('lin', 1, 1, 1)
-        self.assertVectors({'frequency': [1]
-                           ,'a'        : [1]
-                           ,'va#branch': [-2j * pi]
-                           })
+        self.assertVectors({'frequency': [1],
+                            'a'        : [1],
+                            'va#branch': [-2j * pi],
+                            })
 
     def test_inductor(self):
         # operating point does not converge if I use a voltage source here
         ns.circ(['ia a 0 ac 1 dc 0', 'l1 a 0 1'])
         ns.ac('lin', 1, 1, 1)
-        self.assertVectors({'frequency': [1]
-                           ,'a'        : [-2j * pi]
-                           ,'l1#branch': [-1]
-                           })
+        self.assertVectors({'frequency': [1],
+                            'a'        : [-2j * pi],
+                            'l1#branch': [-1],
+                            })
+
 
 class TestCommands(NgspiceTest):
     def test_print(self):
@@ -63,12 +66,12 @@ class TestCommands(NgspiceTest):
         self.assertEqual(ns.plots(),
                          ['op3', 'op2', 'op1', 'const'])
 
-    def test_vectorNames(self):
+    def test_vector_names(self):
         ns.cmd('set curplot = new')
         names = list('abdf')
         for name in names:
             ns.cmd('let {} = 0'.format(name))
-        self.assertEqual(sorted(ns.vectorNames()), names)
+        self.assertEqual(sorted(ns.vector_names()), names)
 
     def test_vector(self):
         ns.cmd('let myvector = unitvec(4)')
@@ -77,7 +80,7 @@ class TestCommands(NgspiceTest):
     def test_alter(self):
         ns.circ('r n 0 1')
         ns.alter('r', resistance=2, temp=3)
-        ns.operating_point() # Necessary for resistance to be calculated
+        ns.operating_point()  # Necessary for resistance to be calculated
         self.assertEqual(ns.vector('@r[resistance]'), 2)
         self.assertEqual(ns.vector('@r[temp]'), 3)
 
@@ -87,9 +90,11 @@ class TestCommands(NgspiceTest):
         ns.operating_point()
         self.assertEqual(ns.vector('@r[resistance]'), 4)
 
+
 class TestHelpers(unittest.TestCase):
     def test_decibel(self):
         self.assertEqual(list(ns.decibel([1, 10, 100])), [0, 10, 20])
+
 
 class TestLinearSweep(unittest.TestCase):
     def setUp(self):
@@ -102,17 +107,17 @@ class TestLinearSweep(unittest.TestCase):
 
     def _test_sweep(self, *args):
         ns.dc('va', *args)
-        self.assertEqualNdarray(ns.vector('a')
-                               ,ns.linear_sweep(*args))
+        self.assertEqualNdarray(ns.vector('a'),
+                                ns.linear_sweep(*args))
 
     def test_linearsweep(self):
         testcases = [
-             (0, 10, 1)
-            ,(0, -10, -1)
-            #,(.3, .3, 0)
-            ,(0, 10, .1)
-            ,(1.23, 4.56, 0.789)
-        ]
+                     (0, 10, 1),
+                     (0, -10, -1),
+                     # (.3, .3, 0),
+                     (0, 10, .1),
+                     (1.23, 4.56, 0.789),
+                     ]
         for sweep in testcases:
             with self.subTest(sweep=sweep):
                 self._test_sweep(*sweep)
